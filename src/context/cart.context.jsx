9a583943a9e7226763ec2a75e8158460a81b3d97
jindev <maiphong1796@gmail.com>
2024-10-23 12:25:45 +0700

    feat: created remove item function, increment item function, decrement function

--- src/context/cart.context.jsx
+++ src/context/cart.context.jsx
@@ -4,31 +4,66 @@ import {createContext, useEffect, useState} from "react";
 const addCartItem = (cartItems, productToAdd) => {
     const existedItem = cartItems.find((item) => item.id === productToAdd.id)
 
-    if(existedItem) {
-       return cartItems.map((cartItem) => cartItem.id === productToAdd.id
-               ? {...cartItem, quantity: cartItem.quantity + 1}
-               : cartItem
-       )
+    if (existedItem) {
+        return cartItems.map((cartItem) => cartItem.id === productToAdd.id
+            ? {...cartItem, quantity: cartItem.quantity + 1}
+            : cartItem
+        )
     }
 
     return [...cartItems, {...productToAdd, quantity: 1}]
 }
 
-export const CartContext  = createContext({
+const removeCartItem = (cartItems, removedItem) => {
+    return cartItems.filter(item => item.id !== removedItem.id)
+}
+
+const incrementItem = (cartItems, item) => {
+    const incrementItemIndex = cartItems.findIndex((itemFromCart) => itemFromCart.id === item.id)
+    if(incrementItemIndex !== -1){
+        cartItems[incrementItemIndex].quantity += 1
+    }
+    return [...cartItems]
+}
+
+const decrementItem = (cartItems, item) => {
+    const decrementItemIndex = cartItems.findIndex((itemFromCart) => itemFromCart.id === item.id)
+    if(decrementItemIndex !== -1 && cartItems[decrementItemIndex].quantity > 0){
+        cartItems[decrementItemIndex].quantity -= 1
+    }
+    return [...cartItems]
+}
+
+export const CartContext = createContext({
     isCartOpen: false,
+    cartItems: [],
+    itemsCount: 0,
     setIsCartOpen: () => {},
     addItemToCart: () => [],
-    cartItems: [],
-    itemsCount: 0
+    removeItemFromCart: () => [],
+    incrementItemFromCart: () => [],
+    decrementItemFromCart: () => []
 });
 
-export const CartContextProvider = ({ children }) => {
+export const CartContextProvider = ({children}) => {
     const [isCartOpen, setIsCartOpen] = useState(false);
     const [cartItems, setCartItems] = useState([]);
     const [itemsCount, setItemsCount] = useState(0)
 
     const addItemToCart = (productToAdd) => {
-       setCartItems(addCartItem(cartItems, productToAdd))
+        setCartItems(addCartItem(cartItems, productToAdd))
+    }
+
+    const removeItemFromCart = (removedItem) => {
+        setCartItems(removeCartItem(cartItems, removedItem))
+    }
+
+    const incrementItemFromCart = (item) => {
+        setCartItems(incrementItem(cartItems, item))
+    }
+
+    const decrementItemFromCart = (item) => {
+        setCartItems(decrementItem(cartItems, item))
     }
 
     useEffect(() => {
@@ -39,7 +74,16 @@ export const CartContextProvider = ({ children }) => {
         setItemsCount(totalCartItem)
     }, [cartItems])
 
-    const value = {isCartOpen, setIsCartOpen,cartItems, addItemToCart, itemsCount}
+    const value = {
+        isCartOpen,
+        cartItems,
+        itemsCount,
+        setIsCartOpen,
+        addItemToCart,
+        removeItemFromCart,
+        incrementItemFromCart,
+        decrementItemFromCart
+    }
 
     return <CartContext.Provider value={value}>{children}</CartContext.Provider>
 }
--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,8 +1,28 @@
 import './checkout.style.scss'
+import {useContext} from "react";
+import {CartContext} from "../../context/cart.context";
+import Button from "../../components/button/button.component";
 
 const Checkout = () => {
+
+    const {cartItems, removeItemFromCart, incrementItemFromCart, decrementItemFromCart} = useContext(CartContext)
+
     return <div>
-        This is checkout page
+        <div>
+            {cartItems.map((cartItem) => {
+                const {name, id, quantity} = cartItem
+                return <div key={id}>
+                    <h2>{name}</h2>
+                    <span>{quantity}</span>
+                    <br/>
+                    <Button onClick={() => decrementItemFromCart(cartItem)}>decrement</Button>
+                    <br/>
+                    <Button onClick={() => incrementItemFromCart(cartItem)}>Increment</Button>
+                    <br/>
+                    <Button onClick={() => removeItemFromCart(cartItem)}>Delete</Button>
+                </div>
+            })}
+        </div>
     </div>
 }
 
