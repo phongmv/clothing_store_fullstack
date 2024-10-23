274537ab8e425e3399c249ca9b82f52804782715
jindev <maiphong1796@gmail.com>
2024-10-23 14:12:11 +0700

    refactor: optimize the code function

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -4,20 +4,26 @@ import {CartContext} from "../../context/cart.context";
 
 const CheckoutItem = ({cartItem}) => {
     const { removeItemFromCart, incrementItemFromCart, decrementItemFromCart} = useContext(CartContext)
-
     const {name, imageUrl, price, quantity} = cartItem
+
+    const handleIncrementItem = () => incrementItemFromCart(cartItem)
+    const handleDecrementItem = () => decrementItemFromCart(cartItem)
+    const handleRemoveItem = () => removeItemFromCart(cartItem)
+
+
+
     return <div className="checkout-item-container">
         <div className="image-container">
             <img src={imageUrl} alt={name}/>
         </div>
         <span className="name">{name}</span>
         <span className="quantity">
-            <span onClick={() => decrementItemFromCart(cartItem)} className="chevron-icon">&#10094;</span>
+            <span onClick={handleDecrementItem} className="chevron-icon">&#10094;</span>
             <span className="quantity-detail">{quantity}</span>
-            <span onClick={() =>  incrementItemFromCart(cartItem)} className="chevron-icon">&#10095;</span>
+            <span onClick={handleIncrementItem} className="chevron-icon">&#10095;</span>
         </span>
         <span className="price">${price}</span>
-        <div onClick={() => removeItemFromCart(cartItem)} className="remove-button">&#10005;</div>
+        <div onClick={handleRemoveItem} className="remove-button">&#10005;</div>
     </div>
 }
 
